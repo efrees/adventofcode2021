e6574647d950aea2d7e3a998fa655c92a2bde1ee
Eric Frees <efrees@atlasrfid.com>
2021-12-03 00:00:08 -0600

    Reduce cruftiness in day 3 solution

--- csharp/AdventOfCode2021/Solvers/Day03Solver.cs
+++ csharp/AdventOfCode2021/Solvers/Day03Solver.cs
@@ -22,13 +22,9 @@ namespace AdventOfCode2021.Solvers
         private static long GetPart1Answer(List<string> lines)
         {
             var mostCommonBits = GetMostCommonBits(lines);
-            var result1 = mostCommonBits
-                .Select(on => on
-                    ? '1'
-                    : '0')
-                .ToArray();
+            var result1 = mostCommonBits;
             var result2 = mostCommonBits
-                .Select(on => on
+                .Select(bit => bit == '1'
                     ? '0'
                     : '1')
                 .ToArray();
@@ -41,34 +37,19 @@ namespace AdventOfCode2021.Solvers
 
         private static long GetPart2Answer(List<string> lines)
         {
+            var numberOfBits = lines.First().Length;
             var oxygenLines = lines.ToList();
-            for (var i = 0; i < lines.First().Length; i++)
+            for (var i = 0; i < numberOfBits && oxygenLines.Count > 1; i++)
             {
-                var oneCounts = GetOneBitCounts(oxygenLines);
-                oxygenLines = oxygenLines.Where(line =>
-                    oneCounts[i] * 2 >= oxygenLines.Count && line[i] == '1'
-                    || oneCounts[i] * 2 < oxygenLines.Count && line[i] == '0'
-                ).ToList();
-
-                if (oxygenLines.Count == 1)
-                {
-                    break;
-                }
+                var mostCommonBits = GetMostCommonBits(oxygenLines);
+                oxygenLines = oxygenLines.Where(line => mostCommonBits[i] == line[i]).ToList();
             }
 
             var co2Lines = lines.ToList();
-            for (var i = 0; i < lines.First().Length; i++)
+            for (var i = 0; i < numberOfBits && co2Lines.Count > 1; i++)
             {
-                var oneCounts = GetOneBitCounts(co2Lines);
-                co2Lines = co2Lines.Where(line =>
-                    oneCounts[i] * 2 >= co2Lines.Count && line[i] == '0'
-                    || oneCounts[i] * 2 < co2Lines.Count && line[i] == '1'
-                ).ToList();
-
-                if (co2Lines.Count == 1)
-                {
-                    break;
-                }
+                var mostCommonBits = GetMostCommonBits(co2Lines);
+                co2Lines = co2Lines.Where(line => mostCommonBits[i] != line[i]).ToList();
             }
 
             var oxygenRating = Convert.ToInt32(new string(oxygenLines.Single()), 2);
@@ -77,33 +58,24 @@ namespace AdventOfCode2021.Solvers
             return oxygenRating * co2Rating;
         }
 
-        private static IEnumerable<bool> GetMostCommonBits(List<string> lines)
+        private static char[] GetMostCommonBits(List<string> lines)
         {
             var oneCounts = GetOneBitCounts(lines);
 
-            var mostCommonBits = Enumerable.Range(0, lines.First().Length)
-                .Select(position => oneCounts[position] >= lines.Count / 2);
-            return mostCommonBits;
+            return Enumerable.Range(0, lines.First().Length)
+                .Select(position => oneCounts[position] * 2 >= lines.Count)
+                .Select(on => on ? '1' : '0')
+                .ToArray();
         }
 
         private static Dictionary<int, int> GetOneBitCounts(List<string> lines)
         {
             var oneCounts = new Dictionary<int, int>();
+            var numberOfBits = lines.First().Length;
 
-            foreach (var line in lines)
+            for (var i = 0; i < numberOfBits; i++)
             {
-                for (var i = 0; i < line.Length; i++)
-                {
-                    if (!oneCounts.ContainsKey(i))
-                    {
-                        oneCounts[i] = 0;
-                    }
-
-                    var bit = line[i];
-
-                    if (bit == '1')
-                        oneCounts[i]++;
-                }
+                oneCounts[i] = lines.Count(line => line[i] == '1');
             }
 
             return oneCounts;
