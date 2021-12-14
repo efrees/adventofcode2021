cbde1b170e6bdd5b2fa55ecb3e6a9d0f8b3a3e19
Eric Frees <efrees@atlasrfid.com>
2021-12-13 23:58:34 -0600

    Solve day 14, part 2 (original)
    This is pretty messy, which perhaps illustrates how little I've practiced
    the thought process of dynamic programming.

--- csharp/AdventOfCode2021/Solvers/Day14Solver.cs
+++ csharp/AdventOfCode2021/Solvers/Day14Solver.cs
@@ -18,7 +18,7 @@ namespace AdventOfCode2021.Solvers
                 .ToList();
 
             Console.WriteLine($"Output (part 1): {GetPart1Answer(lines)}");
-            //Console.WriteLine($"Output (part 2): {GetPart2Answer(lines)}");
+            Console.WriteLine($"Output (part 2): {GetPart2Answer(lines)}");
         }
 
         private static int GetPart1Answer(List<string> input)
@@ -58,6 +58,99 @@ namespace AdventOfCode2021.Solvers
             sb.Append(template.Last());
 
             return sb.ToString();
+        }
+
+        private static long GetPart2Answer(List<string> input)
+        {
+            var template = input.First();
+
+            var rules = new Dictionary<(char, char), char>();
+            foreach (var rule in input.Skip(2))
+            {
+                var splits = rule.Split(" -> ");
+                rules.Add((splits[0][0], splits[0][1]), splits[1].Single());
+            }
+
+            var uniqueLetters = input.SelectMany(line => line).Where(char.IsLetter).Distinct().ToArray();
+            var resultingCounts = new Dictionary<(char, char, int), Dictionary<char, long>>();
+            foreach (var first in uniqueLetters)
+            {
+                foreach (var second in uniqueLetters)
+                {
+                    resultingCounts.Add((first, second, 0), SingleCountsDictionary(first, second));
+                }
+            }
+
+            for (var i = 1; i <= 40; i++)
+            {
+                foreach (var first in uniqueLetters)
+                {
+                    foreach (var second in uniqueLetters)
+                    {
+                        if (!rules.ContainsKey((first, second)))
+                        {
+                            resultingCounts.Add((first, second, i), SingleCountsDictionary(first, second));
+                        }
+                        else
+                        {
+                            var inserted = rules[(first, second)];
+                            var leftCounts = resultingCounts[(first, inserted, i - 1)];
+                            var rightCounts = resultingCounts[(inserted, second, i - 1)];
+                            var combined = CombineCounts(leftCounts, rightCounts);
+                            combined[inserted]--; //remove overlap;
+                            resultingCounts.Add((first, second, i), combined);
+                        }
+                    }
+                }
+            }
+
+            var totalCounts = new Dictionary<char, long>();
+            foreach (var pair in template.Zip(template.Skip(1)))
+            {
+                var counts = resultingCounts[(pair.First, pair.Second, 40)];
+                totalCounts = CombineCounts(totalCounts, counts);
+            }
+
+            foreach (var ch in template[1..^1])
+            {
+                totalCounts[ch]--;
+            }
+
+            var totals = totalCounts.Values.OrderBy(x => x).ToList();
+            return totals.Last() - totals.First();
+        }
+
+        private static Dictionary<char, long> SingleCountsDictionary(char char1, char char2)
+        {
+            if (char1 == char2)
+            {
+                return new Dictionary<char, long>
+                {
+                    { char1, 2 }
+                };
+            }
+
+            return new Dictionary<char, long>
+            {
+                { char1, 1 },
+                { char2, 1 }
+            };
+        }
+
+        private static Dictionary<char, long> CombineCounts(Dictionary<char, long> firstCounts, Dictionary<char, long> secondCounts)
+        {
+            var newFirst = new Dictionary<char, long>(firstCounts);
+            foreach (var (ch, value) in secondCounts)
+            {
+                SafeAddToCount(newFirst, ch, value);
+            }
+
+            return newFirst;
+        }
+
+        private static void SafeAddToCount(Dictionary<char, long> totalCounts, char ch, long value)
+        {
+            totalCounts[ch] = totalCounts.GetValueOrDefault(ch, 0) + value;
         }
     }
 }
